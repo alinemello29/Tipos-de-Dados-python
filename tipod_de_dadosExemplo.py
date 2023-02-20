2b23b0671fe9d898494338e4748fd623ed6193b8
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2023-02-20 15:13:48 -0300

    Add files via upload

---
+++ tipod_de_dadosExemplo.py
@@ -0,0 +1,8 @@
+#tipos em python os tipos built-in são#
+str #texto#
+int, float(),complex() #numerico#
+list(), tuple(), range, #sequência#
+dict #mapa#
+set(), fronzenset, #coleção#
+bool #booleano#
+bytes(), bytearray(), memoryview(obj), #binario#
